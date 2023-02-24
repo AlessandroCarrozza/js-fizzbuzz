e525b96b464031f7bf5f42a907ec31189c01f6b3
Alessandro Carrozza <alessandrowcarrozza@gmail.com>
2023-02-24 16:04:08 +0100

    Squares nel DOM tramite js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
+
+const containerDom = document.getElementById("container");
+
+
+
 for (let num = 1; num <= 100; num++) {
+
+    const square = `<div class="square">testo</div>`;
+    containerDom.innerHTML += square;
     
     if (num%3 == 0 && num%5 == 0) {
         console.log("fizzbuzz");
