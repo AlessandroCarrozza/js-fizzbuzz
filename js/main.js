2b2136d9011b4928d8afa2ba1795fba55bf2553f
Alessandro Carrozza <alessandrowcarrozza@gmail.com>
2023-02-24 16:27:00 +0100

    Bonus completati

--- js/main.js
+++ js/main.js
@@ -7,17 +7,29 @@ for (let num = 1; num <= 100; num++) {
 
     
     if (num%3 == 0 && num%5 == 0) {
-        const square = `<div class="square red">FizzBuzz</div>`;
-        containerDom.innerHTML += square;
+        const square = document.createElement("div");
+        square.append("FizzBuzz");
+        square.classList.add("square");
+        square.classList.add("red");
+        containerDom.append(square);
     } else if (num%5 == 0) {
-        const square = `<div class="square yellow">Buzz</div>`;
-        containerDom.innerHTML += square;
+        const square = document.createElement("div");
+        square.append("Buzz");
+        square.classList.add("square");
+        square.classList.add("yellow");
+        containerDom.append(square);
     } else if (num%3 == 0) {
-        const square = `<div class="square green">Fizz</div>`;
-        containerDom.innerHTML += square;
+        const square = document.createElement("div");
+        square.append("Fizz");
+        square.classList.add("square");
+        square.classList.add("green");
+        containerDom.append(square);
     } else {
-        const square = `<div class="square blue">${num}</div>`;
-        containerDom.innerHTML += square;
+        const square = document.createElement("div");
+        square.append(num);
+        square.classList.add("square");
+        square.classList.add("blue");
+        containerDom.append(square);
     }
 
 }
