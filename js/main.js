9fc7690c100d9878e0d567423e629d1605df4d5a
Alessandro Carrozza <alessandrowcarrozza@gmail.com>
2023-02-24 15:16:09 +0100

    Lavorazione dei multipli nella console

---
+++ js/main.js
@@ -0,0 +1,16 @@
+for (let num = 1; num <= 100; num++) {
+    
+    console.log(num);
+
+    if (num%3 == 0) {
+        console.log("fizz");
+    } else if (num%5 ==0) {
+        console.log("buzz");
+    } else if (num%3 == 0 && num%5 == 0) {
+        console.log("fizzbuzz");
+    }
+
+}
+
+
+
